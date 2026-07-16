fab6f69eec7586d98c6daa9311cce69c80864fd1
muyou <2725077832@qq.com>
2026-07-16 14:56:57 +0800

    fix: 修复uni-app构建配置问题

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,9 +14,8 @@
 
 <style>
 	/*每个页面公共css */
-	@import "node_modules/vant-weapp/dist/common/index.wxss";
 	/* 全局引入IconFont */
-	@import "static/iconfont/iconfont.css";
+	@import "./static/iconfont/iconfont.css";
 	/* 引入uni-icons CSS */
-	@import "node_modules/@dcloudio/uni-ui/lib/uni-icons/uniicons.css";
+	@import "~@dcloudio/uni-ui/lib/uni-icons/uniicons.css";
 </style>
