65fd4cb6b39b85840fac50fb851561096d5762ee
muyou <2725077832@qq.com>
2026-07-16 15:14:52 +0800

    feat: uni-app H5版本构建成功

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,8 +14,18 @@
 
 <style>
 	/*每个页面公共css */
-	/* 全局引入IconFont */
-	@import "./static/iconfont/iconfont.css";
-	/* 引入uni-icons CSS */
-	@import "~@dcloudio/uni-ui/lib/uni-icons/uniicons.css";
 </style>
+
+<script>
+export default {
+  onLaunch: function() {
+    console.log('App Launch')
+  },
+  onShow: function() {
+    console.log('App Show')
+  },
+  onHide: function() {
+    console.log('App Hide')
+  }
+}
+</script>
---
+++ frontend/src/config/live-config.js
@@ -0,0 +1,5 @@
+// 直播配置
+module.exports = {
+  LIVE_STREAM_URL: 'rtmp://live.example.com/stream/test',
+  WS_URL: 'wss://live-fullstack-gateway.onrender.com/ws'
+};
